9a38d1f87649d2a81a908d9c09a13910622a3f86
mkosikov <mJdwOVeBV4pW>
2022-08-22 12:49:00 +0300

    fix: ReWrite Algorithm PivotIndex

--- LeetCodeСhallenge/LeetCode/PivotIndexTask.cs
+++ LeetCodeСhallenge/LeetCode/PivotIndexTask.cs
@@ -1,33 +1,21 @@
-﻿namespace LeetCodeСhallenge.LeetCode;
+﻿
+
+namespace LeetCodeСhallenge.LeetCode;
 
 public static class PivotIndexTask
 {
-    public static int  PivotIndex(int[] nums)
+    public static int PivotIndex(int[] nums)
     {
-        var numsLength = nums.Length;
-        
-        if (IsFirstPivotIndex(nums)) return 0;
+        var leftSum = 0;
+        var allSum = nums.Sum();
+        var lenghtNum = nums.Length;
 
-        if (IsLastPivotIndex(nums)) return numsLength - 1;
-
-        var firstIndex = 1;
-        var lastIndex = nums.Length - 2;
-
-        for (int i = firstIndex; i <= lastIndex; i++)
+        for (int i = 0; i < lenghtNum; i++)
         {
-            var firstPart = nums.SkipLast(numsLength - i).Sum();
-            var secondPart = nums.Skip(i + 1).Sum();
-
-            if (firstPart == secondPart)
-            {
-                return i;
-            }
+            if (leftSum == allSum - leftSum - nums[i]) return i;
+            leftSum += nums[i];
         }
 
         return -1;
     }
-
-    private static bool IsFirstPivotIndex(int[] nums) => 0 == nums.Skip(1).Sum();
-
-    private static bool IsLastPivotIndex(int[] nums) => 0 == nums.SkipLast(1).Sum();
 }
