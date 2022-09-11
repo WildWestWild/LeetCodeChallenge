a50160a1c1cd001503362ef515763188f76ecf8a
mkosikov <mJdwOVeBV4pW>
2022-09-11 20:14:41 +0300

    feat: Valid Parentheses

---
+++ LeetCodeСhallenge/LeetCode/ValidParenthesesTask.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeСhallenge.LeetCode;
+
+public class ValidParenthesesTask
+{
+    public bool IsValid(string s)
+    {
+        var stackChars = new Stack<char>();
+        stackChars.Push(s[0]);
+        
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (!stackChars.Any())
+            {
+                stackChars.Push(s[i]);
+                continue;
+            }
+            
+            var parenthese = stackChars.Peek();
+            if (CheckСouple(parenthese, s[i]))
+                stackChars.Pop();
+            else
+                stackChars.Push(s[i]);
+        }
+
+        return stackChars.Count == default;
+    }
+
+    private bool CheckСouple(char firstP, char secondP)
+    {
+        switch (secondP)
+        {
+            case ')': return firstP == '(';
+            case '}': return firstP == '{';
+            case ']': return firstP == '[';
+            default: return false;
+        }
+    }
+}
